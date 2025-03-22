3d63583b4277aa738068cd0b75cc2b796deb98cf
HC1ab <hwangcs5202@gmail.com>
2025-03-22 15:36:42 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // 'views' 폴더 설정
+app.set('views', './views');
+
+app.use(express.static('public')); // CSS, JS, 이미지 같은 정적 파일 제공
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/testPage1', (req, res) => {
+    res.render('testPage1');
+})
+
+app.get('/testPage2', (req, res) => {
+    res.render('testPage2');
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
