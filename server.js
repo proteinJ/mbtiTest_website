f1585a0e3a21c50717e4cee163a3e7f36eb861ed
unknown <jaehyun516@naver.com>
2025-05-02 14:01:25 +0900

    onrender server PORT fixed

--- server.js
+++ server.js
@@ -139,6 +139,6 @@ app.get('/resultPage', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 })
